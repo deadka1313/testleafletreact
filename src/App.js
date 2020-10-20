031f246a8be95bb24f771c93ac0c48ddd355a6ae
Alexey Malyshev <a.malyshev@magdv.com>
2020-10-20 22:00:33 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-geosearch/assets/css/leaflet.css'
+import { Map as LeafletMap, TileLayer, withLeaflet } from 'react-leaflet';
+import { SearchControl, OpenStreetMapProvider } from 'react-leaflet-geosearch';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const prov = new OpenStreetMapProvider();
+    const GeoSearchControlElement = withLeaflet(SearchControl);
+    return (
+        <div className="App">
+            <LeafletMap center={[51.505, -0.09]} zoom={13} style={{ height: "700px", width: "100%" }}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+                <GeoSearchControlElement
+                    provider={prov}
+                    showMarker= {true}
+                    showPopup={false}
+                    popupFormat={({ query, result }) => result.label}
+                    retainZoomLevel= {false}
+                    animateZoom= {true}
+                    autoClose= {false}
+                    searchLabel={'Enter address, please'}
+                    keepResult= {true}
+                />
+            </LeafletMap>
+        </div>
+    );
 }
 
 export default App;
